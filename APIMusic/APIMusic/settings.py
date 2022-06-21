c4207da07b3af8394a844c45bd7788bddfcf04b1
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-21 15:24:42 -0500

    feat(Django-APIMusic): 'api' app was updated
    'urls.py' module has been added

--- APIMusic/APIMusic/settings.py
+++ APIMusic/APIMusic/settings.py
@@ -44,7 +44,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    'api'
+    'api',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ APIMusic/api/urls.py
@@ -0,0 +1,17 @@
+"""
+This module contains URL Configuration about api app
+"""
+
+__author__ = "Ricardo Robledo"
+__version__ = "0.1"
+
+
+from django.urls import path
+
+
+urlpatterns = [
+    
+]
+
+
+
