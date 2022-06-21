3d3d71629f1e11ccb54225d03dec57d269ec8459
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-21 16:47:46 -0500

    feat(api): 'api' app was updated
    'models.py' module has been added with 'Artist' class

--- APIMusic/api/models.py
+++ APIMusic/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+__author__ = "Ricardo Robledo"
+__version__ = "0.1"
+
+
+# -----------------------------------------------------------------
+#                             Artist
+# -----------------------------------------------------------------
+
+class Artist(models.Model):
+    """
+    This class define our Artist model
+    
+    Attributes:
+        name (str): Name of an artist.
+        middle_name (str): Middle name of an artist.
+        last_name (str): last_name of an artist.
+        age (int):  Age of an artist.
+        genre (str): Genre of an artist.
+        role (str): Role of an artist.
+        created_at (datetime): Date of sign in of the artist in the database.
+        band (str): Band of our artist.
+    """
+
+
+    class Genre(models.TextChoices):
+        """
+        This inner class define our choices for a genre of an artist
+    
+        Attributes:
+            FEMALE tuple(str): Choice for a female artist.
+            MALE tuple(str): Choice for a male artist.
+        """
+        
+        FEMALE = ('F', 'Female')
+        MALE = ('M', 'Male')
+
+
+    name = models.CharField(max_length=15)
+    middle_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    age = models.IntegerField()
+    genre = models.CharField(
+        max_length=1,
+        choices=[Genre.FEMALE, Genre.MALE]
+    )
+    role = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    band = models.CharField(max_length=15)
+    
+    
+    def __str__(self):
+        return self.name
