5510c2dd83640a07bd8021070368e5fee996f64a
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-22 13:33:46 -0500

    feat(api): 'api' app was updated
    'Song' class has been added in 'models.py' module

--- APIMusic/api/models.py
+++ APIMusic/api/models.py
@@ -22,7 +22,7 @@ class Artist(models.Model):
         genre (str): Genre of an artist.
         role (str): Role of an artist.
         created_at (datetime): Date of sign in of the artist in the database.
-        band (str): Band of our artist.
+        band (int): Identifier of a band.
     """
 
 
@@ -119,6 +119,35 @@ class Album(models.Model):
     
     album_name = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.album_name
+
+
+# -----------------------------------------------------------------
+#                             Song
+# -----------------------------------------------------------------
+
+
+class Song(models.Model):
+    """
+    This class define our Song model
+    
+    Attributes:
+        song_name (str): Song name.
+        duration_in_minutes (str): Duration of the song in minutes.
+        created_at (datetime): Date of sign in of the album in the database.
+        band (int): Identifier of a band.
+        album (int): Identifier of an album.
+    """
+
+    
+    song_name = models.CharField(max_length=15)
+    duration_in_minutes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    aband = models.IntegerField()
+    album = models.IntegerField()
     
     
     def __str__(self):
