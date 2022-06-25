e75d132fcdc72f437b16f8dd02789b5df28f1eed
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-25 17:20:29 -0500

    feat(api): 'views.py' module was modified
    'ArtistViewSet' class was added

--- APIMusic/api/views.py
+++ APIMusic/api/views.py
@@ -1,9 +1,30 @@
 from .models import Album, Artist, Band, Song
 from rest_framework import viewsets
-from .serializers import AlbumSerializer
+from .serializers import(
+    AlbumSerializer,
+    ArtistSerializer,
+    BandSerializer,
+    SongSerializer
+)
+
+
+# -----------------------------------------------------------------
+#                             Album
+# -----------------------------------------------------------------
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
 
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+
+
+# -----------------------------------------------------------------
+#                            Artist
+# -----------------------------------------------------------------
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
