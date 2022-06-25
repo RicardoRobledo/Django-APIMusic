9d4b22caa8ebf2974d362448f0cb371b13f57e55
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-25 17:17:09 -0500

    feat(api): 'views.py' module was modified
    'AlbumViewSet' class was added

--- APIMusic/api/views.py
+++ APIMusic/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Album, Artist, Band, Song
+from rest_framework import viewsets
+from .serializers import AlbumSerializer
 
-# Create your views here.
+
+class AlbumViewSet(viewsets.ModelViewSet):
+
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
