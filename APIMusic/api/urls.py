fd9c00b753541ac0b45275eb901c604d47820454
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-25 17:37:31 -0500

    feat(api): 'urls.py' module was modified
    router class was added for all viewsets

--- APIMusic/api/urls.py
+++ APIMusic/api/urls.py
@@ -2,16 +2,26 @@
 This module contains URL Configuration about api app
 """
 
+
 __author__ = "Ricardo Robledo"
 __version__ = "0.1"
 
 
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from .views import(
+    AlbumViewSet,
+    ArtistViewSet,
+    BandSerializer,
+    SongViewSet
+)
 
 
-urlpatterns = [
-    
-]
+# paths
+router = SimpleRouter()
+router.register('', ArtistViewSet, basename='artist')
+router.register('album', AlbumViewSet, basename='album')
+router.register('band', BandSerializer, basename='band')
+router.register('song', SongViewSet, basename='song')
 
-
-
+urlpatterns = router.urls
