99087a89b3c4f571c9659aa51824f446ab5c1a05
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-25 17:05:23 -0500

    feat(api): 'serializers.py' module was modified
    'AlbumSerializer' class was added

--- APIMusic/api/serializers.py
+++ APIMusic/api/serializers.py
@@ -5,3 +5,24 @@ This module define our serializers
 
 __author__ = "Ricardo Robledo"
 __version__ = "0.1"
+
+
+from rest_framework import serializers
+from api.models import Album, Artist, Band, Song
+
+
+# -----------------------------------------------------------------
+#                             Album
+# -----------------------------------------------------------------
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    """
+    Class to serialize Album class
+    """
+
+    
+    class Meta:
+        
+        model = Album
+        fields = ('id', 'album_name', 'created_at',)
