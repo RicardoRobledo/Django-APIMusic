008d07dcb70c22085fe6334d6809db6a97bee245
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-06-25 18:58:16 -0500

    feat(api): 'permissions.py' module has been modified
    'IsAuthorOrReadOnly' class was added

--- APIMusic/api/permissions.py
+++ APIMusic/api/permissions.py
@@ -6,3 +6,14 @@ do in our api
 
 __author__ = "Ricardo Robledo"
 __version__ = "0.1"
+
+
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
