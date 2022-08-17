e0240ac8bccb20d56f51344a3f32d96e446d0de3
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-08-17 15:37:46 -0500

    feat(accounts): 'views.py' module was modified
    'RegisterView', 'LoginView' and 'LogoutView' views were added in 'views.py'

--- APIMusic/APIMusic/urls.py
+++ APIMusic/APIMusic/urls.py
@@ -41,17 +41,18 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('accounts/', include('.urls')),
+    path('accounts/', include('apps.accounts.urls')),
     path('albums/', include('apps.albums.routers')),
     path('bands/', include('apps.bands.routers')),
     path('artists/', include('apps.artists.routers')),
-    #path('users/', include('.urls')),
+    path('users/', include('apps.users.routers')),
     path('api-auth/', include('rest_framework.urls')), # authentification
-    
+
     # simple-jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
+    # swagger
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- APIMusic/apps/accounts/views.py
+++ APIMusic/apps/accounts/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from django.db.models import Q
+from django.contrib.auth import login, logout, authenticate
+from ..users.serializers import UserSerializer
 
-# Create your views here.
+from apps.users.models import User
+
+
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
+class LoginView(TokenObtainPairView):
+    """
+    This class allow us do a login
+    """
+    
+    serializer_class = TokenObtainPairSerializer
+    
+    def post(self, request, *args, **kwargs):
+        """
+        Allow us login
+        
+        :param username: username of our user
+        :param password: password of our user
+        """
+        
+        login_serializer = self.serializer_class(data=request.data)
+        
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+        
+        if login_serializer.is_valid():
+            login(request, user)
+
+        return Response(login_serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    """
+    This class allow us logout
+    """
+
+    def get(self, request, *args, **kwargs):
+        """
+        Allow us logout
+        
+        :param username: username
+        """
+        
+
+        user = User.objects.get(username=request.data.get('username'))
+        
+        if user:
+            logout(request)
+        
+        return Response({'message':'logout success'}, status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+    
+    permission_classes = ()
+    
+    def post(self, request, *args, **kwargs):
+        """
+        Allow us register
+        
+        :param username: username
+        """
+        
+        
+        message = None
+        user_serializer = UserSerializer(data=request.data)
+        
+        user = User.objects.filter(
+            Q(email=request.data['email']) | Q(username=request.data['username'])
+        )
+        
+        if user:
+            message = {'message': 'Existing username or email'}
+        
+        elif user_serializer.is_valid():
+            user_serializer.save()
+            message = {'message':'user created'}
+        
+        else:
+            message = {'message':'wrong record'}
+            
+        
+        return Response(message, status=status.HTTP_200_OK)
