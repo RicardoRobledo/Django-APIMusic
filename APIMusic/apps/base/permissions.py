8296e246f6227e4e532c9471d7672c6a5763c914
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-09-02 20:10:56 -0500

    feat(base): 'permissions.py' module was modified
    documentation in 'IsOwner' class has been added

--- APIMusic/apps/base/permissions.py
+++ APIMusic/apps/base/permissions.py
@@ -2,8 +2,17 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
+    """
+    This class verify if an user has permissions to modify an user
+    """
 
     def has_object_permission(self, request, view, obj):
+        """
+        This method check out if a user can modify, delete or update another user
+
+        Returns:
+            A boolean value that give us if a user can do an action
+        """
 
         if(not request.method in permissions.SAFE_METHODS and obj.id==request.user.id
             or request.user.is_superuser):
